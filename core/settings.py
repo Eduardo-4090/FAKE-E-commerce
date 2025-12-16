10efc52e9301ba7e47bd76d1df712e5dc00137a5
Eduardo-4090 <eduardo007pc@gmail.com>
2025-12-16 15:55:23 -0300

    teste3

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',#
+    'cloudinary',#
     'django.contrib.staticfiles',
     'home',
     'contas',
@@ -120,6 +122,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join( BASE_DIR / 'media')
 
+CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME')
+CLOUDINARY_API_KEY = os.getenv('CLOUDINARY_API_KEY')
+CLOUDINARY_API_SECRET = os.getenv('CLOUDINARY_API_SECRET')
+
+# **LINHA ESSENCIAL:** Define Cloudinary como o sistema de armazenamento padrão
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Detecta HTTPS via proxy do Railway
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
